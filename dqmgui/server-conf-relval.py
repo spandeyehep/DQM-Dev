06e58d2898aeae9e0fb621700d7676dccbb1cb7a
spandeyehep <shubham.pandey@cern.ch>
2016-08-08 13:02:06 +0530

    Added modified Renderplugins and data layout files

--- dqmgui/server-conf-relval.py
+++ dqmgui/server-conf-relval.py
@@ -1,5 +1,8 @@
 import os.path, socket; global CONFIGDIR
 from glob import glob
+
+
+
 CONFIGDIR = os.path.normcase(os.path.abspath(__file__)).rsplit('/', 1)[0]
 BASEDIR   = __file__.rsplit('/', 4)[0]
 STATEDIR  = "%s/state/dqmgui/relval" % BASEDIR
@@ -13,8 +16,14 @@ LAYOUTS += glob("%s/layouts/%smc_relval-layouts.py" % (CONFIGDIR, "tk"))
 LAYOUTS += glob("%s/layouts/%smc_relval-layouts.py" % (CONFIGDIR, "pflow"))
 LAYOUTS += glob("%s/layouts/%s_relval-layouts.py" % (CONFIGDIR, "hlt"))
 LAYOUTS += glob("%s/layouts/%s_relval-layouts.py" % (CONFIGDIR, "ecal"))
+LAYOUTS += glob("%s/layouts/%s_relval-layouts.py" % (CONFIGDIR, "hcal"))  #added by spandey 
 LAYOUTS += glob("%s/layouts/%s_relval-layouts.py" % (CONFIGDIR, "tk"))
 LAYOUTS += glob("%s/layouts/%s_relval-layouts.py" % (CONFIGDIR, "smp"))
+
+
+
+
+
 
 modules = ("Monitoring.DQM.GUI",)
 
